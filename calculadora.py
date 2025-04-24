d0e862c17cd2ce9de30459246576b8397dbe55d2
Andrielson Leonardo Teza <andrielsonteza1@gmail.com>
2025-04-23 23:13:43 -0300

    feat: adiciona função de multiplicação

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,5 @@
-# calculadora.py 
+# calculadora.py - Exemplo para demonstração do Git Flow
+
 def soma(a, b):
     """Realiza a soma de dois números"""
     return a + b
@@ -7,8 +8,13 @@ def subtracao(a, b):
     """Realiza a subtração de dois números"""
     return a - b
 
+def multiplicacao(a, b):
+    """Realiza a multiplicação de dois números"""
+    return a * b
+
 # Função principal
 if __name__ == "__main__":
     print("Calculadora simples")
     print("Soma 5 + 3 =", soma(5, 3))
     print("Subtração 10 - 4 =", subtracao(10, 4))
+    print("Multiplicação 6 * 7 =", multiplicacao(6, 7))
